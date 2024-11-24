bc970cb8282320e7cf205d59bca8f233e81eba5c
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-11-24 11:19:55 +0100

    Add autotag support for angular

--- init.lua
+++ init.lua
@@ -244,7 +244,16 @@ require('lazy').setup({
   {
     'windwp/nvim-ts-autotag',
     config = function()
-      require('nvim-ts-autotag').setup {}
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = true,
+        },
+        aliases = {
+          ['angular.html'] = 'html',
+        },
+      }
     end,
   },
 
