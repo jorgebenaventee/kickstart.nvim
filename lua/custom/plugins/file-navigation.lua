f25f47c5a4764bf04dc166c90b68a489b38711eb
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-12-18 19:41:39 +0100

    Rollback to oil

--- lua/custom/plugins/file-navigation.lua
+++ lua/custom/plugins/file-navigation.lua
@@ -1,17 +1,14 @@
 return {
-  'echasnovski/mini.files',
+  'stevearc/oil.nvim',
   opts = {},
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function()
-    local window = vim.api.nvim_get_current_win()
-    local window_width = vim.api.nvim_win_get_width(window)
-    require('mini.files').setup {
-      windows = { preview = true, width_preview = window_width * 0.5 },
-    }
+    require('oil').setup()
     vim.keymap.set('n', '<leader>oi', function()
-      MiniFiles.open()
-    end, { desc = 'Open file tree' })
+      local oil = require 'oil'
+      oil.open_float()
+    end, { desc = 'Open in Oil' })
   end,
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
